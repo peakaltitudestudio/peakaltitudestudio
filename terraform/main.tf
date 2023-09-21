de287054deb1c6067bc6cd00a7fc8f59f7a36979
peakaltitudestudio <adamlarson@peakaltitudestudio.com>
2023-09-20 23:28:54 -0700

    docker and terraform security group ports, fixpipe

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,21 @@ resource "aws_instance" "pas-website-ec2-instance" {
     Name = "pas-instance"
   }
 }
+
+resource "aws_security_group" "allow-ssh-security-group" {
+  name = "allow-ssh"
+
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+}
+
+resource "aws_network_interface_sg_attachment" "security-group-attachment" {
+  security_group_id    = "${aws_security_group.allow-ssh-security-group.id}"
+  network_interface_id = "${aws_instance.pas-website-ec2-instance.primary_network_interface_id}"
+}
