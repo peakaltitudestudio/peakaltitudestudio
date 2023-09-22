a619202da3a3d06c6744bda61c4ab62ff0f25964
peakaltitudestudio <adamlarson@peakaltitudestudio.com>
2023-09-21 23:29:59 -0700

    updated key pair

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "pas-website-ec2-instance" {
   ami           = "ami-073e64e4c237c08ad"  # Replace with your desired AMI
   instance_type = "t2.micro"              # Replace with your desired instance type
-  key_name      = "ec2keypair"    # Replace with your EC2 key pair name
+  key_name      = "ec2sshkeypair"    # Replace with your EC2 key pair name
 
   tags = {
     Name = "pas-instance"
