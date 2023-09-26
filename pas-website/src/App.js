c2535e43ac9d59f73b1e10fe73d6955e10cefc92
peakaltitudestudio <adamlarson@peakaltitudestudio.com>
2023-09-25 23:47:28 -0700

    Add insta and phone

--- pas-website/src/App.js
+++ pas-website/src/App.js
@@ -1,10 +1,24 @@
+import logo from './pas-logo.svg';
 import './App.css';
 
+//colors: primary: #2f4d54
+//        secondary: #c4a068
+
 function App() {
+  const instagramURL = 'https://www.instagram.com/peak.altitude.studio/'
   return (
     <div className="App">
       <header className="App-header">
-        Peak Altitude Studio
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Peak Altitude Studio</p>
+        <a style={{color:'#c4a068', 'text-decoration': 'none'}} href={instagramURL} target="_blank" rel="noopener noreferrer">
+          Instagram: peak.altitude.studio
+        </a>
+        <p>Phone #: (602)529-2499</p>
+        <br></br>
+        <br></br>
+        <br></br>
+        <p>This site is currently under development - Last updated: Sept 26, 2023</p>
       </header>
     </div>
   );
