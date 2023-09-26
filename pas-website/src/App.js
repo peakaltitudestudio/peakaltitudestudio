b510ade4d860e16631281678a654d35aaa155857
peakaltitudestudio <adamlarson@peakaltitudestudio.com>
2023-09-25 22:31:34 -0700

    remove logo

--- pas-website/src/App.js
+++ pas-website/src/App.js
@@ -1,11 +1,9 @@
-import logo from './pas-logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         Peak Altitude Studio
       </header>
     </div>
