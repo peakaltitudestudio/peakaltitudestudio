745b56ccd9f8511e0576d56f6bde0baaae5adfeb
peakaltitudestudio <adamlarson@peakaltitudestudio.com>
2023-10-10 23:04:42 -0700

    juliette

--- pas-website/src/App.js
+++ pas-website/src/App.js
@@ -1,26 +1,19 @@
-import logo from './pas-logo.svg';
 import './App.css';
 
 //colors: primary: #2f4d54
-//        secondary: #c4a068
+//secondary: #c4a068
 
 function App() {
-  const instagramURL = 'https://www.instagram.com/peak.altitude.studio/'
+  const ringURL = 'https://junerings.com/products/antique-cut-elongated-cushion-solitaire?_pos=5&_sid=10e00d20b&_ss=r'
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Peak Altitude Studio</p>
-        <a style={{color:'#c4a068', 'text-decoration': 'none'}} href={instagramURL} target="_blank" rel="noopener noreferrer">
-          Instagram: peak.altitude.studio
+        <a style={{color:'pink', 'text-decoration': 'none'}} href={ringURL} target="_blank" rel="noopener noreferrer">
+          Juliette
         </a>
-        <p>Phone #: (602)529-2499</p>
         <br></br>
         <br></br>
         <br></br>
-        <p>This site is currently under development</p>
-        <br></br>
-        <p>Last updated: October 10, 2023</p>
       </header>
     </div>
   );
