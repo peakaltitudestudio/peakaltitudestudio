bb2dcad0b2e75b12db0c0c43d732b04e1e894bc8
peakaltitudestudio <adamlarson@peakaltitudestudio.com>
2023-10-10 22:57:59 -0700

    main website

--- pas-website/src/App.js
+++ pas-website/src/App.js
@@ -5,20 +5,22 @@ import './App.css';
 //        secondary: #c4a068
 
 function App() {
-  const instagramURL = 'https://www.instagram.com/couchgoblintimmy/'
+  const instagramURL = 'https://www.instagram.com/peak.altitude.studio/'
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Peak Altitude Studio</p>
         <a style={{color:'#c4a068', 'text-decoration': 'none'}} href={instagramURL} target="_blank" rel="noopener noreferrer">
-          Happy Birthday Timmy!!! Love you long time!
+          Instagram: peak.altitude.studio
         </a>
         <p>Phone #: (602)529-2499</p>
         <br></br>
         <br></br>
         <br></br>
-        <p>This site is currently under development - Last updated: Sept 26, 2023</p>
+        <p>This site is currently under development</p>
+        <br></br>
+        <p>Last updated: October 10, 2023</p>
       </header>
     </div>
   );
