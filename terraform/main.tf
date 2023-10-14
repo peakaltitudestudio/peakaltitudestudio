76eb8cb61999b74d143f585c00a28e06c43d5cc1
peakaltitudestudio <adamlarson@peakaltitudestudio.com>
2023-10-13 23:55:29 -0700

    shorter name if needed

--- terraform/main.tf
+++ terraform/main.tf
@@ -131,7 +131,7 @@ resource "aws_lb_listener" "http_listener_redirect" {
 }
 
 resource "aws_lb_target_group" "pas_target_group" {
-  name        = "${local.env_noblank}-pas-target-group"
+  name        = "${local.env_noblank}-pas-tg"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.pas_main_vpc.id
