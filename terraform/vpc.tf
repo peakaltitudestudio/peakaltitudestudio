28eb1e24af6b4471148498c87da11226a323043b
peakaltitudestudio <adamlarson@peakaltitudestudio.com>
2023-10-15 09:48:26 -0700

    added more tags and clean up

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,17 +2,13 @@ resource "aws_vpc" "pas_main_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   
-  tags = {
-    Name = "${local.env_noblank}"
-  }
+  tags = "${local.default_tags}"
 }
 
 resource "aws_internet_gateway" "pas_internet_gateway" {
   vpc_id = aws_vpc.pas_main_vpc.id
 
-  tags = {
-    Name = "${local.env_noblank}"
-  }
+  tags = "${local.default_tags}"
 }
 
 resource "aws_route" "pas_internet_gateway_route" {
