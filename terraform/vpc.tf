9aed4081585966ea4a32ab225c4101076e3f7937
peakaltitudestudio <adamlarson@peakaltitudestudio.com>
2023-10-14 23:57:07 -0700

    remove a tag

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -19,8 +19,4 @@ resource "aws_route" "pas_internet_gateway_route" {
   route_table_id         = aws_vpc.pas_main_vpc.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.pas_internet_gateway.id
-
-  tags = {
-    Name = "${local.env_noblank}"
-  }
 }
