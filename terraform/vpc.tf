0fad0908b6690d5330f25409ddc0213f1ad19b0c
peakaltitudestudio <adamlarson@peakaltitudestudio.com>
2023-10-14 23:55:05 -0700

    adding more tags

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,14 +1,26 @@
 resource "aws_vpc" "pas_main_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
+  
+  tags = {
+    Name = "${local.env_noblank}"
+  }
 }
 
 resource "aws_internet_gateway" "pas_internet_gateway" {
   vpc_id = aws_vpc.pas_main_vpc.id
+
+  tags = {
+    Name = "${local.env_noblank}"
+  }
 }
 
 resource "aws_route" "pas_internet_gateway_route" {
   route_table_id         = aws_vpc.pas_main_vpc.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.pas_internet_gateway.id
+
+  tags = {
+    Name = "${local.env_noblank}"
+  }
 }
