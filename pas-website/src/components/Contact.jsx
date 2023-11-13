05090cf4bf3300b95cc2e4084dce9131a72ea2ef
peakaltitudestudio <adamlarson@peakaltitudestudio.com>
2023-11-13 16:53:18 -0700

    fix contact

--- pas-website/src/components/Contact.jsx
+++ pas-website/src/components/Contact.jsx
@@ -12,17 +12,19 @@ const instagramURL = 'https://www.instagram.com/peak.altitude.studio/'
 const Home = () => {
     return (
         <div>
-            <PASNavbar/>
-            <div className='logo-content'>
-                <img className="pas-logo" src={logo} alt=""></img>
-                <div className='content'>
-                    <a className="ig" href={instagramURL} target="_blank" rel="noopener noreferrer">
-                        <img className="ig-logo" src={igLogo} alt=""></img>
-                        @peak.altitude.studio
-                    </a>
-                    <br />
-                    <img src={phoneSVG} alt=""></img>
+            <PASNavbar />
+            <div className='contact-main'>
+                <a className="ig" href={instagramURL} target="_blank" rel="noopener noreferrer">
+                    <img className="ig-logo" src={igLogo} alt=""></img>
+                    @peak.altitude.studio
+                </a>
+                <br />
+                <div className='contact-phone'>
+                    <img className='phone-icon' src={phoneSVG} alt=""></img>
                     <p>(602)529-2499</p>
+                </div>
+                <div className="contact-pas-logo-container">
+                    <img className='contact-pas-logo' src={logo} alt=""></img>
                 </div>
             </div>
         </div>
