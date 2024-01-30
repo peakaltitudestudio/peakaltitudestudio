4f634e2a7b274a870209815daf5c024cdd52bef5
peakaltitudestudio <adamlarson@peakaltitudestudio.com>
2024-01-30 10:40:31 -0700

    fix cert naming

--- terraform/main.tf
+++ terraform/main.tf
@@ -135,7 +135,7 @@ resource "aws_lb_listener" "https_listener_forward_www" {
   port              = 443
   protocol          = "HTTPS"
   ssl_policy = "ELBSecurityPolicy-2016-08"
-  certificate_arn = aws_acm_certificate.www_cert.arn
+  certificate_arn = aws_acm_certificate.www_pas_acm_cert.arn
 
   default_action {
     type             = "forward"
